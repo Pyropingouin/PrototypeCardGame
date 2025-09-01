c4d6bf9dd5bcbb4a93344301643581ed7f9c2f5b
Cédrik Lampron <cedrik.lampron@usherbrooke.ca>
2025-09-01 11:04:12 -0400

    inital commit with file from Godot

---
+++ card.gd
@@ -0,0 +1,17 @@
+extends Node2D
+
+signal hovered
+signal hovered_off
+
+var starting_position
+
+func _ready() -> void:
+	#All cards must be a child of CardManager or this will error
+	get_parent().connect_card_signal(self)
+
+func _on_area_2d_mouse_entered() -> void:
+	emit_signal("hovered", self) 
+
+
+func _on_area_2d_mouse_exited() -> void:
+	emit_signal("hovered_off", self)
---
+++ deck.gd
@@ -0,0 +1,8 @@
+extends Node2D
+
+var player_deck = ["Knigh","Knigh", "Knigh"]
+
+
+
+func draw_card():
+	print("draw Card")
