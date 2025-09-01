05ae0be9e4b7f8b9dfb010dfe2295acb8bf7f3e3
Pyropingouin <88858653+Pyropingouin@users.noreply.github.com>
2025-09-01 16:49:58 -0400

    End of tutorial #6
    https://www.youtube.com/watch?v=L1dEuHr5AGU&list=PLNWIwxsLZ-LMYzxHlVb7v5Xo5KaUV7Tq1&index=6

---
+++ CardDatabase.gd
@@ -0,0 +1,5 @@
+const CARDS = { #Attack, Health, Image
+	"Knight" : [2,3, "res://image/Illustration_sans_titre 16.png"],
+	"Archer" : [1,1, "res://image/Illustration_sans_titre 14.png"],
+	"Demon" : [5,7, "res://image/Illustration_sans_titre 18.png"]
+}
--- deck.gd
+++ deck.gd
@@ -3,18 +3,21 @@ extends Node2D
 const CARD_SCENE_PATH = "res://Scenes/Card.tscn"
 const CARD_DRAW_SPEED = 0.2
 
-var player_deck = ["Knight","Knight", "Knight"]
+var player_deck = ["Knight","Archer", "Demon", "Knight"]
+var card_database_reference
 
 
 func _ready() ->void:
+	player_deck.shuffle()
 	$RichTextLabel.text = str(player_deck.size()) 	
+	card_database_reference = preload("res://CardDatabase.gd")
 
 
 
 func draw_card():
 	
-	var card_drawn = player_deck[0]
-	player_deck.erase(card_drawn)
+	var card_drawn_name = player_deck[0]
+	player_deck.erase(card_drawn_name)
 	
 	if player_deck.size() == 0:
 		$Area2D/CollisionShape2D.disabled = true
@@ -24,6 +27,11 @@ func draw_card():
 	$RichTextLabel.text = str(player_deck.size()) 	
 	var card_scene = preload(CARD_SCENE_PATH)
 	var new_card = card_scene.instantiate()
+
+	new_card.get_node("Attack").text = str(card_database_reference.CARDS[card_drawn_name][0])
+	new_card.get_node("Health").text = str(card_database_reference.CARDS[card_drawn_name][1])
+	new_card.get_node("CardImage").texture = load(str(card_database_reference.CARDS[card_drawn_name][2]))
+
 	$"../CardManager".add_child(new_card)
 	new_card.name = "Card"
 	$"../PlayerHand".add_card_to_hand(new_card, CARD_DRAW_SPEED)
